ace3d514c0ae9fbd15c7ad4f0fb44b9d38aa9c80
Audielite <emailemail.com>
2018-03-13 20:15:49 -0500

    first commit

---
+++ routes/index.js
@@ -0,0 +1,30 @@
+var express = require('express');
+var router = express.Router();
+var exchangeRates = require('../model/currencyDB');
+
+/*GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index');
+});
+
+router.get('/convert', function(req, res, next){
+
+  var dollars = req.query.dollars;
+  var toCurrency = req.query.to_currency;
+
+  var converted = dollars * exchangeRates[toCurrency];
+  //console.log(exchangeRates);
+  res.render('results', {
+  dollars: dollars,
+  toCurrency: toCurrency,
+  converted: converted}
+);
+
+});
+
+router.get('/about', function(req, res, next) {
+  res.render('about', {name: "my awesome site!"});
+});
+
+
+module.exports = router;
