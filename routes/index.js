68b9c44e0e1108db6d9eafe5070130e6cc62aa0b
Audielite <emailemail.com>
2018-03-20 00:08:23 -0500

    Some changes made for the Lab

--- routes/index.js
+++ routes/index.js
@@ -6,11 +6,14 @@ var exchangeRates = require('../model/currencyDB');
 router.get('/', function(req, res, next) {
   res.render('index');
 });
-
+/* GET convert page */
 router.get('/convert', function(req, res, next){
-
+  // Dollar amount for converting
   var dollars = req.query.dollars;
+  // Convert to a different currency
   var toCurrency = req.query.to_currency;
+
+  var fromCurrency = req.query.from_Currency;
 
   var converted = dollars * exchangeRates[toCurrency];
   //console.log(exchangeRates);
